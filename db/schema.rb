ee05918792935eeb002342a8920b63bd60687066
Ryan <rquinn.zk@gmail.com>
2010-07-06 01:07:25 -0600

    added empty state for new punchy users

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20100704230608) do
   end
 
   add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
 
   create_table "tags", :force => true do |t|
     t.string "name"
